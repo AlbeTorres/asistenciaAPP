560315f155dda891ab6124963c1c6d19597c5ea6
AlbeTorres <albertocorreoficial@gmail.com>
2021-11-18 09:30:59 -0500

    react router working

--- src/container/App.jsx
+++ src/container/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Footer from '../components/Footer';
 import Main from '../pages/main';
+import {Route,BrowserRouter,Switch} from 'react-router-dom'
 
 
 
@@ -11,8 +12,13 @@ const App = () =>{
 
     
     return(<div className='App'>
-                <Main/>
-                <Footer/>
+    <BrowserRouter>
+        <Switch>
+            <Route exact path= '/main' component={Main}/>
+
+        </Switch>
+    </BrowserRouter>
+    <Footer/>
             
             </div>
     );}
